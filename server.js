bc87001a3bc5306d8219e9149dc0a07907e90e81
Kunal Sharma <85027477+Venom9182@users.noreply.github.com>
2024-09-23 00:53:11 +0530

    adding node modules

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'your_email@gmail.com',
+        pass: 'your_email_password'
+    }
+});
+
+app.post('/send', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: 'kunal.2297.k@gmail.com',
+        subject: `Message from ${name}`,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
